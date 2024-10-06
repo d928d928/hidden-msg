39ac8d566cc75ffb121bdd22cc5d61cd6143bcf2
kunisoft <kunisoft2000@gmail.com>
2024-10-06 23:19:32 +0900

    FirebaseによるGoogle認証ログイン,投稿内容保管機能実装

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from './components/Home';
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import CreatePost from "./components/CreatePost";
+import { useState } from "react";
+import Navbar from "./components/Navbar"
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
   return (
     <Router>
+      <Navbar isAuth={isAuth} />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />}></Route>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
+        <Route path="/logout" element={<Logout setIsAuth={setIsAuth} />}></Route>
       </Routes>
-    </Router>
+    </Router >
   );
 }
 
