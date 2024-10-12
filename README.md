1c3bb141742e901901bc125501277f130fa93cb4
kunisoft <kunisoft2000@gmail.com>
2024-10-13 07:18:09 +0900

    envファイル修正

--- README.md
+++ README.md
@@ -10,3 +10,11 @@
 - グループ機能
   - 閉鎖されたグループ内での利用想定
 - WebとAndroidで実装
+
+# コマンド
+npm i dotenv
+
+git filter-branch --force --index-filter \
+'git rm --cached --ignore-unmatch path/to/your/file' \
+--prune-empty --tag-name-filter cat -- --all
+
--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERId,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
